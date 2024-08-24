48b6dc363ac0c23780e9dca5c57b8d0affd05df8
arhansuba <subasiarhan3@gmail.com>
2024-08-24 22:35:33 +0300

    commit 3

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,10 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  paths: {
+    sources: "./contracts", // Default path for Solidity files
+    tests: "./test", // Default path for test files
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
